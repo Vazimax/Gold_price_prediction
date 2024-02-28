1e102fa548e27742b42ef8f28ff8e0bf50b50125
vazimax <aboubakr.elhabti@gmail.com>
2024-02-28 18:27:52 +0800

    small changes

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
 data = pd.read_csv('gld_price_data.csv')
-
 
 # print(data.head())
 # print(data.tail())
@@ -15,7 +15,7 @@ data = pd.read_csv('gld_price_data.csv')
 # print(data.isnull().sum())
 # print(data.describe())
 
-# =================================================================================
+# ======================================================================
 
 #### Correlation: ####
 
@@ -53,18 +53,14 @@ data = pd.read_csv('gld_price_data.csv')
 X = data.drop(['Date','GLD','SPX', 'USO', 'EUR/USD'],axis=1)
 Y = data['GLD']
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.15,random_state=2)
-
-#### training the model: #####
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.175,random_state=2)
 
 model = RandomForestRegressor(n_estimators=100)
 
 model.fit(X_train,Y_train)
 
-test_data_prediction = model.predict(X_test)
-print(test_data_prediction)
+pred = model.predict(X_test)
+print(pred)
 
-#### Comparing using R squared error: #####
-
-error_score = metrics.r2_score(Y_test, test_data_prediction)
+error_score = metrics.r2_score(Y_test, pred)
 print(error_score)
